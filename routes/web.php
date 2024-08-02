a68b13e59c887493421bd2cc912e84d98094ccaf
DeaNabella <deanabella26@gmail.com>
2024-08-02 14:51:46 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .env.example
    	modified:   .gitignore
    	deleted:    161203@gmail.com
    	modified:   README.md
    	deleted:    app/Http/Controllers/BarangController.php
    	deleted:    app/Http/Controllers/SupplierController.php
    	deleted:    app/Models/Barang.php
    	deleted:    app/Models/Supplier.php
    	new file:   bengkel_db
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/cache.php
    	modified:   config/database.php
    	modified:   config/filesystems.php
    	modified:   config/logging.php
    	modified:   config/mail.php
    	deleted:    database/migrations/2024_05_14_042658_create_barangs_table.php
    	deleted:    database/migrations/2024_05_14_064555_remove_id_barang_from_suppliers.php
    	new file:   database/migrations/2024_07_24_002216_create_customers_table.php
    	new file:   database/migrations/2024_07_24_002228_create_pegawai_table.php
    	new file:   database/migrations/2024_07_24_002243_create_kendaraan_table.php
    	new file:   database/migrations/2024_07_24_002254_create_barang_table.php
    	renamed:    database/migrations/2024_05_14_042717_create_suppliers_table.php -> database/migrations/2024_07_24_002307_create_supplier_table.php
    	new file:   database/migrations/2024_07_24_002315_create_keluhan_table.php
    	deleted:    package-lock.json
    	modified:   package.json
    	new file:   pemesananbengkel
    	deleted:    resources/views/barangs/create.blade.php
    	deleted:    resources/views/barangs/edit.blade.php
    	deleted:    resources/views/barangs/form.blade.php
    	deleted:    resources/views/barangs/index.blade.php
    	deleted:    resources/views/barangs/show.blade.php
    	deleted:    resources/views/layouts/app.blade.php
    	deleted:    resources/views/suppliers/create.blade.php
    	deleted:    resources/views/suppliers/edit.blade.php
    	deleted:    resources/views/suppliers/index.blade.php
    	deleted:    resources/views/suppliers/show.blade.php
    	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\SupplierController;
 
-// Menampilkan halaman welcome
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Redirect rute akar ke halaman daftar barang
-Route::redirect('/','/barangs');
-
-// Barang routes
-Route::resource('barangs', BarangController::class);
-
-// Supplier routes
-Route::resource('suppliers', SupplierController::class);
-
-
